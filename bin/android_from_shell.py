d9d1698017583fd97aca4a931b3a4622b652db18
Jan Helbling <jh@jan-helbling.ch>
2020-11-01 22:44:58 +0100

    small bugfix, wrong apk-path

--- bin/android_from_shell.py
+++ bin/android_from_shell.py
@@ -131,8 +131,8 @@ class build:
 		print("Now Sign the APK!")
 		if not os.path.exists("../mykey.keystore"):
 			print("EXECUTE: keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048")
-			print("EXECUTE: {}/zipalign -f 4 ./{}/bin/app.unaligned.apk ../app.apk".format(BT,NAME))
-			print("EXECUTE: {}/apksigner sign --ks mykey.keystore ../app.apk".format(BT,NAME))
+			print("EXECUTE: {}/zipalign -f 4 ./{}/bin/app.unaligned.apk app.apk".format(BT,NAME))
+			print("EXECUTE: {}/apksigner sign --ks mykey.keystore app.apk".format(BT,NAME))
 		else:
 			self.zipalign_4()
 			print("Enter keystore-password:")
