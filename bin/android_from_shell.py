01aa7b78b077b929e13efcf9572e2cbf8ebfdf02
Jan Helbling <jh@jan-helbling.ch>
2020-11-01 19:27:33 +0100

    update

--- bin/android_from_shell.py
+++ bin/android_from_shell.py
@@ -74,16 +74,31 @@ public class MainActivity extends Activity {
 </resources>""")
 		
 		with open('HelloAndroid/AndroidManifest.xml','w') as fd:
-			fd.write("""<?xml version='1.0'?>
-<manifest xmlns:a='http://schemas.android.com/apk/res/android' package='com.example.helloandroid' a:versionCode='0' a:versionName='0'>
-    <application a:label='A Hello Android'>
-        <activity a:name='com.example.helloandroid.MainActivity'>
-             <intent-filter>
-                <category a:name='android.intent.category.LAUNCHER'/>
-                <action a:name='android.intent.action.MAIN'/>
-             </intent-filter>
-        </activity>
-    </application>
+			fd.write("""<?xml version="1.0" encoding="utf-8"?>
+<manifest
+  xmlns:android=
+    "http://schemas.android.com/apk/res/android"
+  package="com.example.helloandroid"
+  android:versionCode="1"
+  android:versionName="1.0" >
+  <uses-sdk
+    android:minSdkVersion="29"
+    android:targetSdkVersion="29" />
+  <application
+    android:allowBackup="true"
+    android:label="@string/app_name" >
+    <activity
+      android:name=
+        ".MainActivity"
+      android:label="@string/app_name" >
+      <intent-filter>
+        <action android:name=
+          "android.intent.action.MAIN" />
+        <category android:name=
+          "android.intent.category.LAUNCHER" />
+      </intent-filter>
+    </activity>
+  </application>
 </manifest>""")
 		
 		with open('HelloAndroid/res/layout/activity_main.xml','w') as fd:
@@ -110,10 +125,15 @@ class build:
 		self.cp()
 		self.aapt_3()
 		print("Now Sign the APK!")
-		print("EXECUTE: keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048")
-		print("EXECUTE: {}/apksigner sign --ks mykey.keystore ./HelloAndroid/bin/hello.unaligned.apk".format(BT))
-		print("EXECUTE: {}/zipalign -f 4 ./HelloAndroid/bin/hello.unaligned.apk hello.apk".format(BT))
-	
+		if not os.path.exists("../mykey.keystore"):
+			print("EXECUTE: keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048")
+			print("EXECUTE: {}/apksigner sign --ks mykey.keystore ./HelloAndroid/bin/hello.unaligned.apk".format(BT))
+			print("EXECUTE: {}/zipalign -f 4 ./HelloAndroid/bin/hello.unaligned.apk hello.apk".format(BT))
+		else:
+			print("Enter keystore-password:")
+			os.system("{}/apksigner sign --ks ../mykey.keystore ./bin/hello.unaligned.apk".format(BT))
+			self.zipalign_4()
+			print("DONE! hello.apk created!")
 	def aapt_1(self):
 		os.system('{}/aapt package -f -m -J ./src -M ./AndroidManifest.xml -S ./res -I {}/android.jar'.format(BT,PF))
 	
@@ -129,8 +149,11 @@ class build:
 	def cp(self):
 		os.system('cp ./bin/classes.dex .')
 	
-	def aapt_3(sellf):
+	def aapt_3(self):
 		os.system('{}/aapt add ./bin/hello.unaligned.apk classes.dex'.format(BT))
+	
+	def zipalign_4(self):
+		os.system("{}/zipalign -f 4 ../HelloAndroid/bin/hello.unaligned.apk ../hello.apk".format(BT))
 
 if __name__ == '__main__':
 	if sys.argv.__len__() == 1:
